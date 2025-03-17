9a5dae4d11140591f329b8e6de5b97b7a437c5c7
LeeHoin <hobanghann@gmail.com>
2025-03-17 19:25:06 +0900

    try 2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from priority_queue import PriorityQueue
 def main():
   pq = PriorityQueue()
-  
+
   pq.push("Task 1", 3)
   pq.push("Task 2", 1)
   pq.push("Task 3", 2)
@@ -11,4 +11,4 @@ def main():
   print(pq.pop())
 
 if __name__ == "__main__":
-    main()
+  main()
